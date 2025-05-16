7878f7c6137f9d2f58bc3787a80aeaeb2909b9ac
westwood846 <PapstDonB@Googlemail.com>
2025-05-16 19:07:02 +0200

    Add modes for diffing dates and getting a duration from a date

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Duration, type DurationLikeObject } from "luxon";
+import { DateTime, Duration, type DurationLikeObject } from "luxon";
 import "./App.css";
 
 const tokensToUnits = {
@@ -15,6 +15,21 @@ const tokensToUnits = {
 } as const;
 
 type Token = keyof typeof tokensToUnits;
+
+const modes = {
+  DATE_DIFFERENCE: "Date Difference",
+  DURATION_FROM_DATE: "Duration from date",
+  DURATIONS: "Durations",
+} as const;
+
+type Mode = keyof typeof modes;
+
+const operations = {
+  PLUS: "Plus",
+  MINUS: "Minus",
+} as const;
+
+type Operation = keyof typeof operations;
 
 const parseDurationInput = (input: string) => {
   const parts = input.split(/([a-zA-Z]+)/);
@@ -65,57 +80,173 @@ function DurationInput({ input, onChange, onFocus }: DurationInputProps) {
   );
 }
 
+const parseDateInput = (input: string) => {
+  return DateTime.fromISO(input);
+};
+
+const formatDate = (date: DateTime) => {
+  return date.toFormat("F");
+};
+
+interface DateInputProps {
+  input: string;
+  onChange: (input: string) => void;
+  onFocus: () => void;
+}
+
+function DateInput({ input, onChange, onFocus }: DateInputProps) {
+  const date = parseDateInput(input);
+  return (
+    <div className="duration-input">
+      <input
+        type="datetime-local"
+        onChange={(e) => onChange(e.target.value)}
+        onFocus={() => onFocus()}
+        value={input}
+      ></input>
+      <div className="duration-input-display">
+        {date.isValid ? formatDate(date) : date.invalidReason}
+        &nbsp;
+      </div>
+    </div>
+  );
+}
+
+type Inputs = "DATE_A" | "DATE_B" | "DURATION_A" | "DURATION_B";
+
 function App() {
-  const [focusedInput, setFocusedInput] = useState<"A" | "B">("A");
+  const [focusedInput, setFocusedInput] = useState<Inputs>("DURATION_A");
 
   const [durationInputA, setDurationInputA] = useState("");
   const durationA = parseDurationInput(durationInputA);
   const [durationInputB, setDurationInputB] = useState("");
   const durationB = parseDurationInput(durationInputB);
 
-  const setFocusedDuration =
-    focusedInput === "A" ? setDurationInputA : setDurationInputB;
+  const setFocusedDuration = {
+    DURATION_A: setDurationInputA,
+    DURATION_B: setDurationInputB,
+    DATE_A: () => null,
+    DATE_B: () => null,
+  }[focusedInput];
   const focusedDuration =
-    focusedInput === "A" ? durationInputA : durationInputB;
+    focusedInput === "DURATION_A" ? durationInputA : durationInputB;
   const appendToFocusedDuration = (value: string) =>
     setFocusedDuration(focusedDuration + value);
 
-  const [operation, setOperation] = useState<"PLUS" | "MINUS">("PLUS");
+  const [dateInputA, setDateInputA] = useState("");
+  const dateA = parseDateInput(dateInputA);
+  const [dateInputB, setDateInputB] = useState("");
+  const dateB = parseDateInput(dateInputB);
+
+  const [operation, setOperation] = useState<Operation>("PLUS");
+  const [mode, setMode] = useState<Mode>("DURATION_FROM_DATE");
+
+  const setModeAndFocus = (mode: Mode) => {
+    setMode(mode);
+    if (mode === "DURATIONS") setFocusedInput("DURATION_A");
+    if (mode === "DURATION_FROM_DATE") setFocusedInput("DATE_A");
+    if (mode === "DATE_DIFFERENCE") {
+      setFocusedInput("DATE_A");
+      setOperation("MINUS");
+    }
+  };
 
   return (
     <>
       <div className="calc-box">
         <div className="input-box">
-          <DurationInput
-            input={durationInputA}
-            onChange={setDurationInputA}
-            onFocus={() => setFocusedInput("A")}
-          />
+          {mode === "DURATIONS" && (
+            <DurationInput
+              input={durationInputA}
+              onChange={setDurationInputA}
+              onFocus={() => setFocusedInput("DURATION_A")}
+            />
+          )}
+          {(mode === "DURATION_FROM_DATE" || mode === "DATE_DIFFERENCE") && (
+            <DateInput
+              input={dateInputA}
+              onChange={setDateInputA}
+              onFocus={() => setFocusedInput("DATE_A")}
+            />
+          )}
           <div className="operator">{operation === "PLUS" ? "+" : "-"}</div>
-          <DurationInput
-            input={durationInputB}
-            onChange={setDurationInputB}
-            onFocus={() => setFocusedInput("B")}
-          />
+          {(mode === "DURATIONS" || mode === "DURATION_FROM_DATE") && (
+            <DurationInput
+              input={durationInputB}
+              onChange={setDurationInputB}
+              onFocus={() => setFocusedInput("DURATION_B")}
+            />
+          )}
+          {mode === "DATE_DIFFERENCE" && (
+            <DateInput
+              input={dateInputB}
+              onChange={setDateInputB}
+              onFocus={() => setFocusedInput("DATE_B")}
+            />
+          )}
           <div className="operator">=</div>
         </div>
         <div className="result-box">
-          {operation === "PLUS"
-            ? formatDuration(durationA.plus(durationB))
-            : formatDuration(durationA.minus(durationB))}
+          {mode === "DURATIONS" && (
+            <>
+              {durationA.isValid &&
+                durationB.isValid &&
+                (operation === "PLUS"
+                  ? formatDuration(durationA.plus(durationB))
+                  : formatDuration(durationA.minus(durationB)))}
+            </>
+          )}
+          {mode === "DURATION_FROM_DATE" && (
+            <>
+              {dateA.isValid && durationB.isValid && operation === "PLUS"
+                ? formatDate(dateA.plus(durationB))
+                : formatDate(dateA.minus(durationB))}
+            </>
+          )}
+          {mode === "DATE_DIFFERENCE" && (
+            <>
+              {dateA.isValid &&
+                dateB.isValid &&
+                formatDuration(dateA.diff(dateB))}
+            </>
+          )}
           &nbsp;
         </div>
       </div>
 
       <div className="operator-dials-container">
-        <div className="operator-grid">
-          <button onClick={() => setOperation("PLUS")} title="Plus">
-            +
-          </button>
-          <button onClick={() => setOperation("MINUS")} title="Minus">
-            -
-          </button>
-        </div>
+        <fieldset>
+          <legend>Mode</legend>
+          {Object.entries(modes).map(([key, label]) => (
+            <>
+              <input
+                id={`modeRadio${key}`}
+                type="radio"
+                name="mode"
+                value={key}
+                checked={mode === key}
+                onChange={() => setModeAndFocus(key as Mode)}
+              />
+              <label htmlFor={`modeRadio${key}`}>{label}</label>
+            </>
+          ))}
+        </fieldset>
+        <fieldset>
+          <legend>Operation</legend>
+          {Object.entries(operations).map(([key, label]) => (
+            <>
+              <input
+                id={`operatorRadio${key}`}
+                type="radio"
+                name="operator"
+                value={key}
+                checked={operation === key}
+                onChange={() => setOperation(key as Operation)}
+              />
+              <label htmlFor={`operatorRadio${key}`}>{label}</label>
+            </>
+          ))}
+        </fieldset>
       </div>
 
       <div className="dials-container">
